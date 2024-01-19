a7003524e27aa3e61506f13407e6b6078eca8ca4
malwinakochman <malwinakochman@gmail.com>
2024-01-19 02:55:39 +0100

    Formularz od zera, pozdrawiam

--- src/views/NewBillView.vue
+++ src/views/NewBillView.vue
@@ -1,11 +1,49 @@
 <template>
-    <div>
-        new bill form
-    </div>
+  <div>
+    <h2>Dodaj rachunek</h2>
+    <form @submit.prevent="addBill">
+      <input v-model="bill.billPrice" type="number" placeholder="Cena rachunku" />
+      <input v-model="bill.isPayed" type="checkbox" /> Czy zapłacono
+      <input v-model="bill.listOfDishes" type="text" placeholder="Lista dań" />
+      <input v-model="bill.date" type="datetime-local" />
+      <input v-model="bill.waiterId" type="number" placeholder="ID kelnera" />
+      <button type="submit">Dodaj rachunek</button>
+    </form>
+  </div>
 </template>
-<script setup>
 
+<script>
+import api from "@/services/api";
+
+export default {
+  data() {
+    return {
+      bill: {
+        billPrice: null,
+        isPayed: false,
+        listOfDishes: '',
+        date: '',
+        waiterId: null,
+      },
+    };
+  },
+  methods: {
+    async addBill() {
+      try {
+        const response = await api.addBill(this.bill);
+        console.log('Rachunek dodany', response.data);
+        // Reset form
+        this.bill = {
+          billPrice: null,
+          isPayed: false,
+          listOfDishes: '',
+          date: '',
+          waiterId: null,
+        };
+      } catch (error) {
+        console.error('Błąd podczas dodawania rachunku', error);
+      }
+    },
+  },
+};
 </script>
-<style scoped>
-    
-</style>
