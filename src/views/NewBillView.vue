cfc49aa1fe28a6f04c05a9c377098227c95cf3cf
malwinakochman <malwinakochman@gmail.com>
2024-01-19 02:57:42 +0100

    update

--- src/views/NewBillView.vue
+++ src/views/NewBillView.vue
@@ -2,11 +2,11 @@
   <div>
     <h2>Dodaj rachunek</h2>
     <form @submit.prevent="addBill">
-      <input v-model="bill.billPrice" type="number" placeholder="Cena rachunku" />
-      <input v-model="bill.isPayed" type="checkbox" /> Czy zapłacono
       <input v-model="bill.listOfDishes" type="text" placeholder="Lista dań" />
       <input v-model="bill.date" type="datetime-local" />
-      <input v-model="bill.waiterId" type="number" placeholder="ID kelnera" />
+      <input v-model="bill.waiterId" type="number" placeholder="Id Kelnera" />
+      <input v-model="bill.billPrice" type="number" placeholder="Łączny koszt" />
+      <input v-model="bill.isPayed" type="checkbox" /> Zapłacony
       <button type="submit">Dodaj rachunek</button>
     </form>
   </div>
